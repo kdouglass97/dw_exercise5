0c070ccbd5f1122786caee4d7d08d2fdac5cd5a1
kdouglass97 <66433917+kdouglass97@users.noreply.github.com>
2023-11-07 08:30:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const indexRoute = require('.routes/index');
+//const singlePostRoute = require('.routes/singlePost');
+//const createPostRoute = require('.routes/createPost');
+
+//app.use uses two args, function & paramter for path
+app.use("/", indexRoute);
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
