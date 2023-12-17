a7bdd3621c35e4207cb7c250f11ce2a5d5b9e142
kdouglass97 <66433917+kdouglass97@users.noreply.github.com>
2023-12-17 18:27:20 -0500

    added write functionality + changed firebase rules

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const firebase = require("firebase/app");
-//port
+
 const port = 4000;
 
-//MY UNIQUE configuration for firebase
+// web app's Firebase configuration -- make sure this is above the indexRoute
 const firebaseConfig = {
   apiKey: "AIzaSyBRtT5vNBCQ54-6Pm7HQC8GNpJsFHIHglA",
   authDomain: "ktd-dw-exercise5.firebaseapp.com",
@@ -14,15 +14,17 @@ const firebaseConfig = {
   appId: "1:51889770166:web:f12fba6fc498a9bcaaa017"
 };
 
+// initialize firebase
 firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
-//const singlePostRoute = require('.routes/singlePost');
-//const createPostRoute = require('.routes/createPost');
-
-//app.use uses two args, function & paramter for path
+const singlePostRoute = require('./routes/singlePost');
+const createPostRoute = require('./routes/createPost');
+  
 app.use("/", indexRoute);
+app.use("/post", singlePostRoute);
+app.use("/create", createPostRoute);
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+    console.log(`Exercise Five listening on port ${port}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-//Initialize Firestore Database
+// intialize firebase database
 const firestore = require("firebase/firestore");
-
-//create a reference to the database
+// create a reference to the database
 const db = firestore.getFirestore();
 
-//define index route - get all posts
-router.get("/", (req,res) => {
-    const postsQuery = firestore.getDocs(firestore.collection(db,"posts"));
-    const postsArray = []
+// define index route -- get all posts
+router.get("/", (req, res) => {
+    const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+    const postsArray = [];
 
     postsQuery
-    .then((response) => {
-        response.forEach((post) => {
-            console.log(post.data());
-            postsArray.push({ id: post.id, ...post.data() });
-        })
-        res.send(postsArray)
-    })
-    .catch((error) => {
-        console.log(error);
-        return res.send(error);
-    });
+        .then((response) => {
+            response.forEach((post) => {
+                console.log(post.data());
+                // ... is a spread operator, assigns the value throughout rather than creating individual objects for each key value pair
+                postsArray.push({id: post.id, ...post.data()}); 
+            });
+            res.send(postsArray);
+        }) // do not put semi colon -- they are connected
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
 });
+
+const singlePostRoute = require('./singlePost');
+router.use("/post", singlePostRoute);
+const createPostRoute = require('./createPost');
+router.use("/create", createPostRoute);
 
 module.exports = router;
